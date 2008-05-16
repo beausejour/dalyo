b2a2ac1f3d74bcc5da7c56a488b069ec0d911053
roland.hu <roland.hu@4ae9eb9b-c943-0410-ad6f-d31c06732d59>
2008-05-16 16:18:30 +0000

    Add variables declaration support

--- trunk/DMA_Android/src/com/penbase/dma/Dalyo/Function/Function.java
+++ trunk/DMA_Android/src/com/penbase/dma/Dalyo/Function/Function.java
@@ -16,25 +16,29 @@ import com.penbase.dma.xml.XmlTag;
 public class Function {
 	private Document behaviorDocument = ApplicationView.behaviorDocument;
 	private Context context;
-	private HashMap<String, ArrayList<String>> varList;
+	private HashMap<String, ArrayList<String>> varMap;
+	private HashMap<String, Integer> funcMap;
 	
 	public Function(Context c)
 	{
 		this.context = c;
-		varList = new HashMap<String, ArrayList<String>>();
-		createVaraible();
+		varMap = new HashMap<String, ArrayList<String>>();
+		funcMap = new HashMap<String, Integer>();
+		createMaps();		
 	}
 	
 	/*
-	 * This method need to be deleted if we don't need global variables anymore in Dalyo studio
+	 * This method need to be modified if we don't need global variables anymore in Dalyo studio
+	 * Create 2 maps which contains variables and the positions of function
 	 * */
-	public void createVaraible()
+	public void createMaps()
 	{
 		NodeList funcList = behaviorDocument.getElementsByTagName(XmlTag.TAG_SCRIPT_FUNCTION);
 		int functionSize = funcList.getLength();
 		for (int i=0; i<functionSize; i++)
 		{
 			Element funcElement = (Element) funcList.item(i);
+			funcMap.put(funcElement.getAttribute(XmlTag.TAG_SCRIPT_NAME), i);			
 			NodeList nodesList = funcElement.getChildNodes();
 			int itemLen = nodesList.getLength();
 			for (int j=0; j<itemLen; j++)
@@ -44,22 +48,21 @@ public class Function {
 				{
 					if (element.hasChildNodes())
 					{
-						varList.put(element.getAttribute(XmlTag.TAG_SCRIPT_NAME), 
+						varMap.put(element.getAttribute(XmlTag.TAG_SCRIPT_NAME), 
 								setVariable(element.getAttribute(XmlTag.TAG_SCRIPT_TYPE),
 										element.getChildNodes().item(0).getNodeValue()));
 					}
 				}
 			}
 		}
-	}
+	}	
 	
-	public void createFunction(String name)
+	public void createFunction(String name, NodeList params)
 	{
 		NodeList funcList = behaviorDocument.getElementsByTagName(XmlTag.TAG_SCRIPT_FUNCTION);
-		int functionSize = funcList.getLength();
-		for (int i=0; i<functionSize; i++)
+		if (funcMap.containsKey(name))
 		{
-			Element funcElement = (Element) funcList.item(i);
+			Element funcElement = (Element) funcList.item(funcMap.get(name));
 			if (funcElement.getAttribute(XmlTag.TAG_SCRIPT_NAME).equals(name))
 			{
 				NodeList nodesList = funcElement.getChildNodes();
@@ -69,19 +72,29 @@ public class Function {
 				for (int j=0; j<itemLen; j++)
 				{
 					Element element = (Element) nodesList.item(j);
-					Log.i("info", "element name "+element.getNodeName());
+					Log.i("info", "element name "+element.getNodeName()+" paramater number "+
+							element.getElementsByTagName(XmlTag.TAG_SCRIPT_PARAMETER).getLength());
 					if ((element.getNodeName().equals(XmlTag.TAG_SCRIPT_CALL)) && 
 							(element.hasAttribute(XmlTag.TAG_SCRIPT_CALL_FUNCTION)))
 					{
 						if ((element.getAttribute(XmlTag.TAG_SCRIPT_CALL_FUNCTION).equals(XmlScriptAttribute.FUNCTION_ALERT)) ||
-								(element.getAttribute(XmlTag.TAG_SCRIPT_CALL_FUNCTION).equals(XmlScriptAttribute.FUNCTION_ERROR)))
+								(element.getAttribute(XmlTag.TAG_SCRIPT_CALL_FUNCTION).equals(XmlScriptAttribute.FUNCTION_ERROR)) &&
+								(element.getAttribute(XmlTag.TAG_SCRIPT_CALL_NAMESPACE).equals(XmlScriptAttribute.NAMESPACE_RUNTIME)))
 						{
 							Log.i("info", "call showalertdialog");
-							showAlertDialog(element.getChildNodes());
+							showAlertDialog(element.getElementsByTagName(XmlTag.TAG_SCRIPT_PARAMETER), params);						
+							//showAlertDialog(element.getChildNodes(), element.getElementsByTagName(XmlTag.TAG_SCRIPT_PARAMETER));
 						}
-						else if (element.getAttribute(XmlTag.TAG_SCRIPT_CALL_FUNCTION).equals(XmlScriptAttribute.FUNCTION_NAVIGATE))
+						else if ((element.getAttribute(XmlTag.TAG_SCRIPT_CALL_FUNCTION).equals(XmlScriptAttribute.FUNCTION_NAVIGATE)) &&
+								(element.getAttribute(XmlTag.TAG_SCRIPT_CALL_NAMESPACE).equals(XmlScriptAttribute.NAMESPACE_FORM)))
 						{
-							changeContentView(element.getChildNodes());
+							changeContentView(element.getElementsByTagName(XmlTag.TAG_SCRIPT_PARAMETER), params);
+						}
+						else if ((element.getAttribute(XmlTag.TAG_SCRIPT_CALL_NAMESPACE).equals(XmlScriptAttribute.NAMESPACE_USER)) &&
+								(element.getElementsByTagName(XmlTag.TAG_SCRIPT_PARAMETER).getLength() > 0))
+						{
+							createFunction(element.getAttribute(XmlTag.TAG_SCRIPT_CALL_FUNCTION), 
+									element.getElementsByTagName(XmlTag.TAG_SCRIPT_PARAMETER));
 						}
 					}
 					/*
@@ -104,12 +117,10 @@ public class Function {
 	}
 	
 	public Object getVariableValue(String name)
-	{
-		Log.i("info", "get valeu of "+name+" "+varList.size());
-		
-		if (varList.containsKey(name))
+	{		
+		if (varMap.containsKey(name))
 		{
-			ArrayList<String> variable = varList.get(name);
+			ArrayList<String> variable = varMap.get(name);
 			Log.i("info", "value "+variable.get(1));
 			return variable.get(1);
 		}
@@ -127,9 +138,8 @@ public class Function {
 		return array;
 	}
 	
-	public void changeContentView(NodeList items)
-	{
-		ApplicationView.applicationView.setContentView(ApplicationView.layoutList.get(1));
+	public void changeContentView(NodeList items, NodeList params)
+	{		
 		int itemsLen = items.getLength();
 		
 		for (int i=0; i<itemsLen; i++)
@@ -149,13 +159,54 @@ public class Function {
 					{
 						int id = Integer.valueOf(elt.getAttribute(XmlTag.TAG_SCRIPT_ELEMENT_ID));
 						ApplicationView.applicationView.setContentView(ApplicationView.layoutList.get(id-1));
-					}					
+					}
 				}
 			}
 		}
 	}
 	
-	public void showAlertDialog(NodeList items)
+	public String getDialogValue(Element element, String name, NodeList newParams)
+	{
+		String result = "";
+		String type = element.getAttribute(XmlTag.TAG_SCRIPT_TYPE);
+		if (element.getAttribute(XmlTag.TAG_SCRIPT_NAME).equals(name))
+		{
+			NodeList params = element.getChildNodes();
+			int paramsLen = params.getLength();	
+			for (int j=0; j<paramsLen; j++)
+			{
+				if (params.item(j).getNodeType() == Node.ELEMENT_NODE)
+				{
+					Element param = (Element)params.item(j);
+					
+					if (param.getNodeName().equals(XmlTag.TAG_SCRIPT_VAR))
+					{
+						if (getVariableValue(param.getAttribute(XmlTag.TAG_SCRIPT_NAME)) != null)
+						{
+							result = (String) getVariableValue(param.getAttribute(XmlTag.TAG_SCRIPT_NAME));						
+						}
+						else if (getParamValue(newParams, param.getAttribute(XmlTag.TAG_SCRIPT_NAME), type) != null)
+						{
+							result = (String) getParamValue(newParams, param.getAttribute(XmlTag.TAG_SCRIPT_NAME), type);
+						}
+						else if (param.getNodeName().equals(XmlTag.TAG_SCRIPT_KEYWOED))
+						{
+							//check keyword
+							result = param.getChildNodes().item(0).getNodeValue();
+						}
+					}	
+				}						
+				else if (params.item(j).getNodeType() == Node.TEXT_NODE)
+				{
+					result = params.item(j).getNodeValue();
+				}				
+			}
+		}
+
+		return result;
+	}
+	
+	public void showAlertDialog(NodeList items, NodeList newParams)
 	{
 		int itemsLen = items.getLength();
 		String message = "";
@@ -163,51 +214,17 @@ public class Function {
 		
 		for (int i=0; i<itemsLen; i++)
 		{
-			Element element = (Element)items.item(i);			
-			
+			Element element = (Element)items.item(i);
 			if ((element.getNodeName().equals(XmlTag.TAG_SCRIPT_PARAMETER)) &&
 					(element.hasAttribute(XmlTag.TAG_SCRIPT_NAME)))
-			{				
+			{
 				if (element.getAttribute(XmlTag.TAG_SCRIPT_NAME).equals(XmlScriptAttribute.PARAMETER_TEXT))
-				{					
-					NodeList params = element.getChildNodes();
-					int paramsLen = params.getLength();	
-					for (int j=0; j<paramsLen; j++)
-					{
-						if (params.item(j).getNodeType() == Node.ELEMENT_NODE)
-						{
-							Element param = (Element)params.item(j);
-							
-							if (param.getNodeName().equals(XmlTag.TAG_SCRIPT_VAR))
-							{							
-								message = (String) getVariableValue(param.getAttribute(XmlTag.TAG_SCRIPT_NAME));
-							}	
-						}						
-						else if (params.item(j).getNodeType() == Node.TEXT_NODE)
-						{
-							message = params.item(j).getNodeValue();
-						}											
-					}
+				{
+					message = getDialogValue(element, XmlScriptAttribute.PARAMETER_TEXT, newParams);
 				}
 				else if (element.getAttribute(XmlTag.TAG_SCRIPT_NAME).equals(XmlScriptAttribute.PARAMETER_CAPTION))
 				{
-					NodeList params = element.getChildNodes();
-					int paramsLen = params.getLength();
-					
-					for (int j=0; j<paramsLen; j++)
-					{						
-						Element param = (Element)params.item(j);
-						if (param.getNodeName().equals(XmlTag.TAG_SCRIPT_VAR))
-						{							
-							title = (String) getVariableValue(param.getAttribute(XmlTag.TAG_SCRIPT_NAME));
-							Log.i("info", "caption "+title+" "+param.getAttribute(XmlTag.TAG_SCRIPT_NAME));
-						}
-						else if (param.getNodeName().equals(XmlTag.TAG_SCRIPT_KEYWOED))
-						{
-							//check keyword
-							title = param.getChildNodes().item(0).getNodeValue();
-						}											
-					}					
+					title = getDialogValue(element, XmlScriptAttribute.PARAMETER_CAPTION, newParams);
 				}
 			}
 		}
@@ -220,5 +237,25 @@ public class Function {
 		{
 			new AlertDialog.Builder(context).setMessage(message).show();			
 		}
+	}
+	
+	public Object getParamValue(NodeList params, String name, String type)
+	{		
+		Object result = null;
+		int paramLen = params.getLength();
+		
+		for (int i=0; i<paramLen; i++)
+		{
+			Element element = (Element) params.item(i);			
+			if ((element.getAttribute(XmlTag.TAG_SCRIPT_NAME).equals(name)) &&
+					(element.getAttribute(XmlTag.TAG_SCRIPT_TYPE).equals(type)))
+			{
+				if (element.getChildNodes().item(0).getNodeType() == Node.TEXT_NODE)
+				{
+					result = element.getChildNodes().item(0).getNodeValue();
+				}
+			}
+		}
+		return result;
 	}
 }
