e7faafe8b9574f6ae40be1fccc085d3a76e17575
roland.hu <roland.hu@4ae9eb9b-c943-0410-ad6f-d31c06732d59>
2009-01-08 14:35:37 +0000

    Implemented script method:AddMinutes for NS_Date, Division and Round for NS_Math.

--- trunk/DMA_Android/src/com/penbase/dma/Dalyo/Function/DateTime/Date.java
+++ trunk/DMA_Android/src/com/penbase/dma/Dalyo/Function/DateTime/Date.java
@@ -7,19 +7,32 @@ public class Date {
 	private int day;
 	private int month;
 	private int year;
+	private int hour;
+	private int minute;
+	private int second;
+	private Calendar calendar;
 	
 	public Date() {
-		Calendar calendar = Calendar.getInstance();
+		calendar = Calendar.getInstance();
 		this.day = calendar.get(Calendar.DATE);
 		this.month = calendar.get(Calendar.MONTH)+1;
 		this.year = calendar.get(Calendar.YEAR);
+		this.hour = calendar.get(Calendar.HOUR_OF_DAY);
+		this.minute = calendar.get(Calendar.MINUTE);
+		this.second = calendar.get(Calendar.SECOND);
 	}
 	
 	public String toString() {
-		return day+"/"+month+"/"+year;
+		return day+"/"+month+"/"+year+" "+hour+":"+minute+":"+second;
 	}
 	
 	public int toInt() {
-		return new Long(new GregorianCalendar(year, month, day).getTimeInMillis()).intValue();
+		return new Long(new GregorianCalendar(year, month, day, hour, minute, second).getTimeInMillis()).intValue();
+	}
+	
+	public Date addMinutes(int minutes) {
+		calendar.add(Calendar.MINUTE, minutes);
+		this.minute = calendar.get(Calendar.MINUTE);
+		return this;
 	}
 }
