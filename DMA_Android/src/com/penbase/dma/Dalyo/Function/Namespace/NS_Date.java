2d3fd4a65e01dce338bbaaefec685ad7735d69ad
roland.hu <roland.hu@4ae9eb9b-c943-0410-ad6f-d31c06732d59>
2009-02-23 13:43:28 +0000

    Modified DalyoDate object, implemented Format method.

--- DMA_Android/src/com/penbase/dma/Dalyo/Function/Namespace/NS_Date.java
+++ DMA_Android/src/com/penbase/dma/Dalyo/Function/Namespace/NS_Date.java
@@ -1,25 +1,64 @@
 package com.penbase.dma.Dalyo.Function.Namespace;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
 import org.w3c.dom.Element;
 
 import com.penbase.dma.Constant.ScriptAttribute;
 import com.penbase.dma.Constant.ScriptTag;
 import com.penbase.dma.Dalyo.Function.Function;
-import com.penbase.dma.Dalyo.Function.DateTime.Date;
+import com.penbase.dma.Dalyo.Function.DateTime.DalyoDate;
 import com.penbase.dma.Dalyo.Function.DateTime.Time;
 
 public class NS_Date {
 	public static Object AddMinutes(Element element) {
 		Object date = Function.getValue(element, ScriptTag.PARAMETER, ScriptAttribute.DATE, ScriptAttribute.DATE);
 		int minutes = Integer.valueOf(Function.getValue(element, ScriptTag.PARAMETER, ScriptAttribute.PARAMETER_NAME_MINUTES, ScriptAttribute.PARAMETER_TYPE_INT).toString());
-		return ((Date)date).addMinutes(minutes);
+		return ((DalyoDate)date).addMinutes(minutes);
 	}
 	
 	public static Object CurrentDate() {
-		return new Date();
+		return new DalyoDate();
 	}
 	
 	public static Object CurrentHour() {
 		return new Time();
+	}
+	
+	public static String Format(Element element) {
+		Object date = Function.getValue(element, ScriptTag.PARAMETER, ScriptAttribute.DATE, ScriptAttribute.DATE);
+		String format = Function.getValue(element, ScriptTag.PARAMETER, ScriptAttribute.PARAMETER_NAME_FORMAT, ScriptAttribute.STRING).toString();
+		
+		String tempPattern = format;
+		ArrayList<Integer> indexArray = new ArrayList<Integer>();
+		int patternLength = format.length();
+		for (int i=0; i<patternLength; i++) {
+			if (tempPattern.charAt(i) == '\\') {
+				indexArray.add(i);
+			}
+		}
+
+		int indexArraySize = indexArray.size();
+		if (indexArraySize > 0) {
+			for (int i=0; i<indexArraySize; i++) {
+				if (indexArray.get(i) + 1 < format.length()) {
+					tempPattern = tempPattern.replace(tempPattern.charAt(indexArray.get(i) + 1), ' ');
+				}
+			}
+		}
+		
+		Format formatter = new SimpleDateFormat(tempPattern);
+		StringBuffer formatedDate = new StringBuffer(formatter.format(date));
+		
+		for (int i=0; i<indexArraySize; i++) {
+			if (indexArray.get(i) + 1 < format.length()) {
+				String newString = ""+format.charAt(indexArray.get(i) + 1);
+				formatedDate = formatedDate.replace(indexArray.get(i) + 1, indexArray.get(i) + 2, newString);
+			}
+		}
+		
+		return formatedDate.toString().replace("\\", "");
 	}
 }
